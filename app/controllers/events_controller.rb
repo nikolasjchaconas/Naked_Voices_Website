20ef5a34ad90418332f78f581d24b6ff3ca977db
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2016-02-28 10:52:11 -0800

    more changes to how things look

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,11 @@
 class EventsController < ApplicationController
   def show
   	@last_updated = File.mtime("app/views/events/show.html.erb")
+  	Venmo.configure do |c|
+	  c.access_token= "My Super Secret API"
+	  c.privacy= "private"
+	  #private, public, friends are vaild options
+	  #see Venmo documentation
+	end
   end
 end
